dbe4ee41cd21b3f71ebe34c68b25e937108b9934
devonxmurray <murraydevon14@gmail.com>
2023-04-19 12:56:41 +0000

    modified:   App/controllers/auth.py 	modified:   App/templates/layout.html 	modified:   App/views/allworkouts.py

--- App/views/allworkouts.py
+++ App/views/allworkouts.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify, json, flash
 import requests
 allworkouts_views = Blueprint('allworkouts_views', __name__, template_folder='../templates')
-#from App.controllers import ()
+from App.controllers import (
+    get_workout
+)
 
 
 
@@ -10,8 +12,8 @@ def allworkouts_page():
     return render_template('allworkouts.html')
 
 #returns multiple workouts
-@allworkouts_views.route('/search', methods=['POST'])
-def searchworkouts_page():
-    data = request.form
-    workouts = get_workout(data["name"])
-    return render_template('search.html', workouts=workouts) 
+@allworkouts_views.route('/search', methods=['GET'])
+def searchWorkouts_page():
+    query = request.args.get('query')
+    workouts = get_workout(query)
+    return render_template('search.html', workouts=workouts, search=query) 
