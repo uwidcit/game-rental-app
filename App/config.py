f9da11e2a7b7cf5ce8c0519f153b96a45b1edcca
Nicholas Mendez <snickdx@gmail.com>
2024-03-10 14:47:20 +0000

    updated requirements and config

--- App/config.py
+++ App/config.py
@@ -7,7 +7,7 @@ def load_config():
     config = {'ENV': os.environ.get('ENV', 'DEVELOPMENT')}
     delta = 7
     if config['ENV'] == "DEVELOPMENT":
-        from .custom_config import JWT_ACCESS_TOKEN_EXPIRES, SQLALCHEMY_DATABASE_URI, SECRET_KEY, RAWG_TOKEN
+        from .default_config import JWT_ACCESS_TOKEN_EXPIRES, SQLALCHEMY_DATABASE_URI, SECRET_KEY, RAWG_TOKEN
         config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
         config['SECRET_KEY'] = SECRET_KEY
         config['RAWG_TOKEN'] = RAWG_TOKEN
@@ -22,7 +22,6 @@ def load_config():
     config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=int(delta))
     config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     config['TEMPLATES_AUTO_RELOAD'] = True
-    config['SEVER_NAME'] = '0.0.0.0'
     config['PREFERRED_URL_SCHEME'] = 'https'
     config['UPLOADED_PHOTOS_DEST'] = "App/uploads"
     config["JWT_TOKEN_LOCATION"] = ["headers"]
